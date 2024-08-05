bb4e42d70596a04c3777ab5428d2456004de8547
Hemanth <hemanth22092003@gmail.com>
2024-08-05 21:58:10 +0530

    First Commit

---
+++ src/main/java/com/app/demo/CC_Exercise/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.CC_Exercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello! I am Hemanth" );
+    }
+}
